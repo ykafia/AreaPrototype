0601be6de8c44c79b23b14f3f9df4a10445317c3
Youness KAFIA <kafia.youness@hotmail.fr>
2022-01-12 19:20:56 +0100

    Added disc light

---
+++ AreaPrototype/DiscLightRenderer/DiscLightData.cs
@@ -0,0 +1,14 @@
+using Stride.Core.Mathematics;
+
+namespace AreaPrototype
+{
+    public struct DiscLightData
+    {
+        public Vector3 PositionWS;
+        float padding0;
+        public Vector3 DirectionWS;
+        float padding1;
+        public Color3 Color;
+        public float  Range;
+    }
+}
