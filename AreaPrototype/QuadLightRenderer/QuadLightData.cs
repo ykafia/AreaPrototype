eb0d6d83e7e31b5a67439bd948d42a2980ce13ab
Youness KAFIA <kafia.youness@hotmail.fr>
2022-01-16 16:41:08 +0100

    Started  quad light

---
+++ AreaPrototype/QuadLightRenderer/QuadLightData.cs
@@ -0,0 +1,16 @@
+using Stride.Core.Mathematics;
+
+namespace AreaPrototype
+{
+    public struct QuadLightData
+    {
+        public Vector3 PositionWS;
+        float padding0;
+        public Vector3 PlaneNormalWS;
+        float padding1;
+        public Color3 Color;
+        public float Range;
+        public float Radius;
+        public float Intensity;
+    }
+}
